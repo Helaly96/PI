ecb085315c4986701ae20383cbca844a30973ee7
Amr-V <35613645+Amr-V@users.noreply.github.com>
2018-12-15 18:32:51 +0200

    Update UDP.py

--- UDP.py
+++ UDP.py
@@ -1,1 +1,12 @@
-#
+import socket
+class UDP:
+    def __init__(self,targetIp,port):
+        self._targetIp=targetIp
+        self._port=port
+        self.socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    def update(self,event,message):
+        if event == 'SENSOR':
+            message=str(message).encode(encoding="UTF-8")
+            self.socket.sendto(message,(self._targetIp,self._port))
+
