1dd394aec6e51b6ca8f6930357ec8c5e51b90f05
Amr-V <35613645+Amr-V@users.noreply.github.com>
2018-12-17 12:10:07 +0200

    Update DummyHat.py

--- DummyHat.py
+++ DummyHat.py
@@ -3,7 +3,7 @@ class Hat:
     def __init__(self, address, frequency):
         self.address = address
         self.frequency = frequency
-
+        self.emit_signal = None
         self._devices = {}
     # Set the Speed of All Motors
     def add_Device(self,name,channel,zero_value):
@@ -16,6 +16,10 @@ class Hat:
         return self._devices[device_name]['current']
     def setDeviceValue(self, device_name,value):
         self._devices[device_name]['current'] = value
+
+    def SIGNAL_Referance(self,Observer_Pattern_Signal):
+        self.emit_signal=Observer_Pattern_Signal
+
     # pwms is a dict ======> {'Motor1' : value1 , 'Motor2 : value2 ,.... etc }
     def _updatePWM(self,pwms:dict):
         for device_name in self._devices:
@@ -23,8 +27,10 @@ class Hat:
                 continue
             self._devices[device_name]['current'] =pwms[device_name]
         #print(pwms)
+
     def update(self, event_name,pwm):
 
         if event_name == "PWM":
             self._updatePWM(pwm)
+            self.emit_signal("SENSOR",pwm['Vertical_Right'])
 
