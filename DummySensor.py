274a101e5c36f4da42ea189e95420208a9703298
Amr-V <35613645+Amr-V@users.noreply.github.com>
2018-12-17 12:10:31 +0200

    Update DummySensor.py

--- DummySensor.py
+++ DummySensor.py
@@ -1,13 +1,21 @@
 import threading
 class Dummy_Sensor:
-    def __init__(self):
+    def __init__(self,time):
+        self.pwm = 305
         self.pressure = 0
-        self.time = 1
+        self.time = time
+
+    def SIGNAL_Referance(self,Observer_Pattern_Signal):
+        self.emit_signal=Observer_Pattern_Signal
 
     def interrupt(self, update, name):
         self.update_pressure()
-        update(name, self.pressure)
+        update(name,self.pressure,self.pwm)
         threading.Timer(self.time, self.interrupt, [update, name]).start()
 
     def update_pressure(self):
         self.pressure += 1
+    def update_pwm(self,event,z:int):
+        if event == "SENSOR":
+            self.pwm = z
+            print(self.pwm)
